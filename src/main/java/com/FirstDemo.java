764c8df12e80aa2f503831725239b18bd7a0384f
shao Zengwei <shaozengwei@shaodeMacBook-Pro.local>
2022-05-01 09:34:05 +0800

    spark first demo

---
+++ src/main/java/com/FirstDemo.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
+public class FirstDemo {
+    public static void main(String[] args) {
+        SparkSession ss = SparkSession.builder().appName("test").master("local[*]").getOrCreate();
+
+        StructType schema = new StructType(new StructField[]{
+                new StructField("first", DataTypes.StringType, true, Metadata.empty())
+        });
+        ss.read().format("csv").schema(schema).load("input").write().format("json").save("output");
+    }
+}
