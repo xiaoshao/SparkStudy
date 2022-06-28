bba969790fac3c49a2b6adb7a52c65ee3a31f735
shao Zengwei <shaozengwei@shaodeMacBook-Pro.local>
2022-06-28 08:42:41 +0800

    spark study

--- src/main/scala/com/implicitd/ImplicitDemoMain.scala
+++ src/main/scala/com/implicitd/ImplicitDemoMain.scala
@@ -6,14 +6,27 @@ object ImplicitDemoMain {
 
   def main(args: Array[String]): Unit = {
     fun1()
+
+    5.times(println("Hi"))
   }
 
-  def fun1(): Unit ={
+  def fun1(): Unit = {
     implicit val str = "hello world"
 
     showStr
   }
 
+  implicit class Transfer(x: Int) {
+    def times[A](f: => A): Unit = {
+      def loop(current: Int): Unit = {
+        if (current > 0) {
+          f
+          loop(current - 1)
+        }
+      }
 
+      loop(x)
+    }
+  }
 
 }
--- src/main/scala/com/implicitd/ImplicitDemoMain2.scala
+++ src/main/scala/com/implicitd/ImplicitDemoMain2.scala
@@ -3,17 +3,28 @@ package com.implicitd
 object ImplicitDemoMain2 {
 
   def main(args: Array[String]): Unit = {
+    println(sum(List(1, 2, 3)))
 
+    println(sum( List("A", "B", "C")))
   }
 
-  implicit val StringMonoid: Monoid[String] = {
+  implicit val StringMonoid: Monoid[String] = new Monoid[String] {
     def add(x: String, y: String): String = x concat y
 
     def unit: String = ""
   }
 
-  implicit val IntMonoid: Monoid[Int] = {
-    def add(x: Int, y: Int) : Int = x +y
-    def  unit: Int = 0
+  implicit val IntMonoid: Monoid[Int] = new Monoid[Int] {
+    def add(x: Int, y: Int): Int = x + y
+
+    def unit: Int = 0
+  }
+
+  def sum[A](xs: List[A])(implicit m: Monoid[A]): A = {
+    if (xs.isEmpty) {
+      m.unit
+    } else {
+      m.add(xs.head, sum(xs.tail))
+    }
   }
 }
